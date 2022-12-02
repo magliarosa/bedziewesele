d4990d4094508f702511456d1fc72cc8287f640b
Tomasz.Makowski <Tomasz.Makowski@ergohestia.pl>
2022-12-02 02:07:01 +0100

    fix set date

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import DateCountdown from 'react-date-countdown-timer';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       {/* <DateCountdown dateTo='January 01, 2023 00:00:00 GMT+03:00' callback={()=>alert('Hello')}  /> */}
         <p className='WeddingDate'>
           29 | 07 | 2023
         </p>
