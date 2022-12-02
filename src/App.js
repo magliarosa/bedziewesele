81cee3efe7bb5b12b25c136be8b054f2f586120c
Tomasz.Makowski <Tomasz.Makowski@ergohestia.pl>
2022-12-02 02:51:31 +0100

    added countdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import Countdown from 'react-countdown';
+
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    return <span className='Countdown'><div>{days} dni</div> {hours}:{minutes}:{seconds}</span>;
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p className='Initials'>K & T</p>
+        <p>Do ślubu zostało:</p>
+        <Countdown 
+          date={'2023-07-29T15:00:00'}
+          renderer={renderer}/> 
         <p className='WeddingDate'>
           29 | 07 | 2023
         </p>
+        15:00
       </header>
     </div>
   );
