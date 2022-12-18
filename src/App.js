51576a5f457b9ccfd200bade0b29b1a1ea379595
Tomasz.Makowski <Tomasz.Makowski@ergohestia.pl>
2022-12-18 17:51:59 +0100

    added images

--- src/App.js
+++ src/App.js
@@ -2,19 +2,42 @@ import './App.css';
 import Countdown from 'react-countdown';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
+// import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
-  return <span className='Countdown'><div>{days} dni</div> {hours}:{minutes}:{seconds}</span>;
+  return <span className='Countdown'>
+    <div>
+      {days} dni
+    </div>
+    <TimeContainer>
+      <HourContainer><div>{hours}</div><div className='TimeUnit'>g</div></HourContainer>
+      <MinuteContainer><div>{minutes}</div><div className='TimeUnit'>m</div></MinuteContainer>
+      <SecondContainer><div>{seconds}</div><div className='TimeUnit'>s</div></SecondContainer>
+    </TimeContainer>
+  </span>;
 };
 
 const A = styled.a`
-  color: white;
+  width: fit-content;
+  color: black;
+  text-align: left;
   &:hover {
-    color: white;
+    color: black;
   }
   &:visited {
-    color: white;
+    color: black;
   }
+`;
+
+const MiddleImg = styled.img`
+  width: 100%;
+  object-fit: cover;
+`;
+
+const SideImg = styled.img`
+  height: 100%;
+  width: min-content;
 `;
 
 const Links = styled.div`
@@ -22,33 +45,155 @@ const Links = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 10px;
+`;
+
+const SidePicture = styled.div`
+  width: 25vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  background-color: #fefefe;
+  align-content: center;
+  justify-content: center;
+`;
+
+const MiddlePicture = styled.div`
+  max-height: 25%;
+`;
+
+const MainContent = styled.div`
+  /* padding: 20px; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  color: black;
+  font-family: 'Cormorant Garamond', serif;
+  font-weight: 500;
+  font-size: calc(20px + 2vmin);
+  width: 50vw;
+  
+  ::-webkit-scrollbar{
+    width: 5px;
+    background-color: transparent;
+  }
+  ::-webkit-scrollbar-thumb{
+    background-color: #e3b3da;
+    border-radius: 10px;
+  }
+  @media (max-width: 600px) {
+    width: 100%;
+    overflow-y: auto;
+    ::-webkit-scrollbar{
+    width: 0px;
+    background-color: transparent;
+    }    
+    ::-webkit-scrollbar-thumb{
+    background-color: transparent;
+    }
+  }
+`;
+
+const Initials = styled.div`
+  font-size: calc(20px + 5vmin);
+  padding: 1rem;
+  white-space: nowrap;
+`;
+
+const WeddingDate = styled.div`
+  font-size: calc(5px + 4vmin);
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+`;
+
+const WeddingCountdown = styled.div`
+  font-size: calc(10px + 5vmin);
+  border-style: solid;
+  border-width: 0.5px;
+  padding: 20px;
+`
+const TimeContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  font-size: xx-large;
+  row-gap: 10px;
+`;
+const HourContainer = styled.div`
+  width: 33%;
+  display: flex;
+  flex-direction: column;
+`;
+const MinuteContainer = styled.div`
+  width: 33%;
+  display: flex;
+  flex-direction: column;
+`;
+const SecondContainer = styled.div`
+  width: 33%;
+  display: flex;
+  flex-direction: column;
+`;
+
+
+const WeddingApp = styled.div`
+  height: 100vh;
+  margin: 0%;
+  overflow: hidden;
+  background-color: ##fefefe;
+  text-align: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  @media (max-width: 600px) {
+    justify-content: space-around;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-items: center;
+  }
 `
 
-
 function App() {
+  const isMobile = useMediaQuery({ query: '(max-width: 601px)' });
   return (
-    <div className="App">
+    <WeddingApp>
       <Helmet>
         <title>Ślub Kasi i Tomka</title>
       </Helmet>
-      <header className="App-header">
-        <p className='Initials'>K & T</p>
+      {!isMobile && (<SidePicture>
+        <SideImg src='photos/tomek_profil.jpg' alt='123'></SideImg>
+      </SidePicture>)}
+      {isMobile && (<MiddlePicture><MiddleImg src='photos/kasia_gora.jpg' alt='123'></MiddleImg></MiddlePicture>)}
+      <MainContent className='box'>
+        <Initials>K & T</Initials>
         <p>Do ślubu zostało:</p>
-        <Countdown
-          date={new Date(Date.UTC(2023, 7, 29, 14, 0, 0, 0))}
-          renderer={renderer} />
-        <p className='WeddingDate'>
-          29 | 07 | 2023
-        </p>
-        15:00
-      </header>
-      <content>
+        <WeddingCountdown>
+          <Countdown
+            date={new Date(Date.UTC(2023, 7, 29, 15, 0, 0, 0))}
+            renderer={renderer} />
+        </WeddingCountdown>
+        <WeddingDate>
+          <span>29 | 07 | 2023</span>
+          <span>16:00</span>
+        </WeddingDate>
         <Links>
           <A target={'_blank'} href='https://goo.gl/maps/X16KL9MjJcvoeVw39'>Kościół</A>
           <A target={'_blank'} href='https://goo.gl/maps/f5TWSABHb1XdiAks8'>Sala weselna</A>
         </Links>
-      </content>
-    </div>
+      </MainContent>
+      {isMobile && (<MiddlePicture><MiddleImg src='photos/tomek_dol.jpg' alt='123'></MiddleImg></MiddlePicture>)}
+      {!isMobile && (<SidePicture>
+        <SideImg src='photos/kasia_profil.jpg' alt='123'></SideImg>
+      </SidePicture>)}
+    </WeddingApp>
   );
 }
 
